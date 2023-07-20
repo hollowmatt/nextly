895dd9964aba912ff75b943350453baa5bec2e10
Matt Holloway <hollowmatt@gmail.com>
2023-07-20 19:35:43 -0400

    a bit more styling on the home page

--- next/nextblog/pages/index.js
+++ next/nextblog/pages/index.js
@@ -4,6 +4,7 @@ import Layout, { siteTitle } from '../components/layout';
 import { getSortedPostsData } from '../lib/posts';
 import Link from 'next/link';
 import Date from '../components/date';
+import Container from '../components/container';
 
 export async function getStaticProps() {
   const allPostsData = getSortedPostsData();
@@ -20,28 +21,67 @@ export default function Home( {allPostsData} ) {
       <Head>
         <title>{siteTitle}</title>
       </Head>
-      <section className={utilStyles.headingMd}>
-        <p>
-          Hello, I'm <strong>Matt</strong>.  I do stuff at Google.
-        </p>
-        <p>
-          This is basically a simple, random set of blog posts - built with <a href="https://nextjs.org/learn">Next.js</a>.
-        </p>
-      </section>
-      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
-        <h2 className={utilStyles.headingLg}>Blog</h2>
-        <ul className={utilStyles.list}>
-          {allPostsData.map(({ id, date, title }) => (
-            <li className={utilStyles.listItem} key={id}>
-              <Link href={`/posts/${id}`}>{title}</Link>
-              <br/>
-              <small className={utilStyles.lightText}>
-                <Date dateString={date} />
-              </small>
-            </li>
-          ))}
-        </ul>
-      </section>
+      <Container>
+        {/* Move this section to a 'intro' omponent */}
+        <section className="flex-col md:flex-row flex items-center md:justify-between mt-16 mb-16 md:mb-12">
+          <h1 className="text-5xl md:text-8xl font-bold tracking-tighter leading-tight md:pr-8">
+            Blogly.
+          </h1>
+          <h4 className="text-center md:text-left text-lg mt-5 md:pl-8">
+          Hello, I'm <strong>Matt</strong>.  I do stuff at Google.   This is basically a simple, random set of blog posts using {' '}
+            <a
+              href="https://nextjs.org/"
+              className="underline hover:text-blue-600 duration-200 transition-colors"
+            >
+              Next.js
+            </a>{' '}
+            and Markdown.
+          </h4>
+        </section>
+
+        {/* Move this section to a 'hero' component */}
+        <section>
+          <div className="mb-8 md:mb-16">
+            <p>[cover image here]</p>
+          </div>
+          <div className="md:grid md:grid-cols-2 md:gap-x-16 lg:gap-x-8 mb-20 md:mb-28">
+            <div>
+              <h3 className="mb-4 text-4xl lg:text-5xl leading-tight">
+                <Link
+                  as={`/posts/wobbly`}
+                  href="/posts/wobbly"
+                  className="hover:underline"
+                >
+                  Wobbly, Wobbly
+                </Link>
+              </h3>
+              <div className="mb-4 md:mb-0 text-lg">
+                <Date dateString="2023-07-17" />
+              </div>
+            </div>
+            <div>
+              <p className="text-lg leading-relaxed mb-4">[excerpt]</p>
+              [avatar]
+            </div>
+          </div>
+        </section>
+
+        {/* All Posts Component */}
+        <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+          <h2 className={utilStyles.headingLg}>All Posts...</h2>
+          <ul className={utilStyles.list}>
+            {allPostsData.map(({ id, date, title }) => (
+              <li className={utilStyles.listItem} key={id}>
+                <Link href={`/posts/${id}`}>{title}</Link>
+                <br/>
+                <small className={utilStyles.lightText}>
+                  <Date dateString={date} />
+                </small>
+              </li>
+            ))}
+          </ul>
+        </section>
+      </Container>
     </Layout>
   )
 }
