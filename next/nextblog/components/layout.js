0cf51200444f6f0143ee25bd5f178134bec789e8
Matt Holloway <hollowmatt@gmail.com>
2023-07-31 08:37:56 -0400

    move layout images to components

--- next/nextblog/components/layout.js
+++ next/nextblog/components/layout.js
@@ -6,8 +6,11 @@ import utilStyles from '../styles/utils.module.css';
 import Link from 'next/link';
 import Footer from './footer';
 import Container from './container';
+import { LargeAvatar, MedAvatar } from './image';
 const name = "Matt Holloway";
 export const siteTitle = "Matt's Blog - supplemental";
+export const headerImage = "/images/profile.jpg";
+export const headerAltText = "Captain's Blog - Supplemental"
 
 export default function Layout({ children, home }) {
   return (
@@ -31,27 +34,13 @@ export default function Layout({ children, home }) {
         <header className={styles.header}>
           {home ? (
             <>
-              <Image
-                priority
-                src="/images/profile.jpg"
-                className={utilStyles.borderCircle}
-                height={144}
-                width={144}
-                alt="Profile Pic"
-              />
+              <LargeAvatar path={headerImage} altText={headerAltText} />
               <h1 className={utilStyles.heading2Xl}>{name}</h1>
             </>
           ) : (
             <>
               <Link href="/">
-                <Image
-                  priority
-                  src='/images/profile.jpg'
-                  className={utilStyles.borderCircle}
-                  height={108}
-                  width={108}
-                  alt="Smaller Profile Pic"
-                />
+                <MedAvatar path={headerImage} altText={headerAltText} />
               </Link>
               <h2 className={utilStyles.headingLg}>
                 <Link href="/" className={utilStyles.colorInherit}>
