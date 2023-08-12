a2f276210e4661af0dbc85f62f297599fa521fa7
Matt Holloway <hollowmatt@google.com>
2023-08-12 14:21:47 +0000

    remove the add post placeholder for now

--- next/nextblog/pages/index.js
+++ next/nextblog/pages/index.js
@@ -43,7 +43,6 @@ export default function Home( {allPosts, allPostsData, headerTitle, headerConten
         <IntroFire title = {headerTitle} content = {headerContent} />
         <Hero postData={heroPost} />
         <PostList postsData={morePostsData} />
-        <AddPost />
       </Container>
     </Layout>
   )
