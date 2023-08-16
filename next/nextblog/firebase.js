d57365aba4ac0862ac54a605d1dcdab3d32045ac
Matt Holloway <hollowmatt@gmail.com>
2023-08-15 20:21:06 -0400

    move the body of the posts to a cloud bucket

--- next/nextblog/firebase.js
+++ next/nextblog/firebase.js
@@ -1,5 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
@@ -12,3 +13,4 @@ const firebaseConfig = {
 
 export const app = initializeApp(firebaseConfig);
 export const database = getFirestore(app);
+export const storage = getStorage(app);
