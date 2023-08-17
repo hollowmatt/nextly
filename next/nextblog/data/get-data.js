9f14f421f14661a52e03d9436871c87bd94fff6f
Matt Holloway <hollowmatt@google.com>
2023-08-17 01:01:05 +0000

    put together sorted results

--- next/nextblog/data/get-data.js
+++ next/nextblog/data/get-data.js
@@ -1,4 +1,4 @@
-import { collection, getDocs, doc, getDoc } from "firebase/firestore";
+import { collection, getDocs, doc, getDoc, query, orderBy } from "firebase/firestore";
 import { database } from "../firebase";
 //const dbInstance = collection(database, 'managers');
 
@@ -7,6 +7,16 @@ export async function getData(coll) {
   const data = snapshot.docs.map((item) => {
     return { ...item.data(), id: item.id }
   });
+  return data;
+}
+
+export async function getSortedData(coll) {
+  const data = database.collection(coll)
+    .orderBy("date", "desc")
+    .get()
+    .then((snapshot => {
+      return {...snapshot.data(), id: snapshot.id}
+    }));
   return data;
 }
 
