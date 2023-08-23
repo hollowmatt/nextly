a87dfd319604337b758bf43147aa643d2e0b61ce
Matt Holloway <hollowmatt@google.com>
2023-08-23 13:48:37 +0000

    comment file for disqus

---
+++ next/nextblog/pages/comment.js
@@ -0,0 +1,9 @@
+export default function Comment() {
+
+    return (
+      <p>
+        Comment as though you were face to face. Be civil - no hate, no trolling, no cursing, no BS.
+      </p>
+    )
+  }
+  
